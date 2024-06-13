7b24156c9afeac1e6a1da2684a8fef05d9eb38db
jchier <chier@pdx.edu>
2024-06-13 11:35:38 -0700

    updated countImages.py for githubactions

--- countImages.py
+++ countImages.py
@@ -1,11 +1,15 @@
 import os
 
-directory = "./source"
+def main():
 
-onlyfiles = next(os.walk(directory))[2] 
+    directory = "./source"
 
-fo = open("count.txt", "w")
-fo.write(str(len(onlyfiles)))
-fo.close()
+    onlyfiles = next(os.walk(directory))[2] 
+    print(str(len(onlyfiles)))
+    fo = open("count.txt", "w")
+    fo.write(str(len(onlyfiles)))
+    fo.close()
 
 
+if __name__ == '__main__':
+    main()
