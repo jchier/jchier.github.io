70b2ede6dd3ad047d56b8399aa633b0ee7cf2e10
jchier <chier@pdx.edu>
2024-06-12 23:43:23 -0700

    changed page formatting, added forward and back button

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
 let imageSource = "/source/1-1.jpg";
 let countSource = ".index.txt";
+let count = 0;
+let current = 1;
 
-function loadImages(imgArr) {
-  let img = new Image();
-  img.src = imageSource;
-  var frame = document.getElementById("frame");
-  frame.src = imageSource;
-  frame.setAttribute("alt", "lol");
+function nextImage() {
+  if (current < count) {
+    current++;
+    let frame = document.getElementById("frame");
+    frame.src = "/source/1-" + current + ".jpg";
+    frame.setAttribute("alt", "lol");
+    console.log(current);
+  }
+}
+
+function previousImage() {
+  if (current != 1) {
+    current--;
+    let frame = document.getElementById("frame");
+    frame.src = "/source/1-" + current + ".jpg";
+    frame.setAttribute("alt", "lol");
+    console.log(current);
+  }
 }
 
 let btnRight = document.getElementById("btn-right");
 btnRight.addEventListener("click", () => {
-  loadImages();
+  //loadImages();
+  nextImage();
+});
+
+let btnLeft = document.getElementById("btn-left");
+btnLeft.addEventListener("click", () => {
+  //loadImages();
+  previousImage();
 });
 
 async function fetchTextFile() {
   try {
     // Replace 'https://example.com/path/to/yourfile.txt' with the URL of your remote .txt file
-    const response = await fetch("https://example.com/path/to/yourfile.txt");
+    const response = await fetch("https://jchier.github.io/input.txt");
 
     if (!response.ok) {
       throw new Error("Network response was not ok " + response.statusText);
     }
 
     // Get the text content from the response
-    const data = await response.text();
+    count = await response.text();
 
     // Display the content in the HTML element
-    console.log(data);
+    console.log(count);
   } catch (error) {
     console.error("Error fetching the text file:", error);
   }
